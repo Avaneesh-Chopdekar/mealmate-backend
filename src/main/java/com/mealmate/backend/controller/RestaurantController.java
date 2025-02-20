72b92d4640ca171354cc1b341bc770c9809514fa
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-20 12:21:03 +0530

    created acceptOrder() method in RestaurantController class

---
+++ src/main/java/com/mealmate/backend/controller/RestaurantController.java
@@ -0,0 +1,27 @@
+package com.mealmate.backend.controller;
+
+import com.mealmate.backend.entity.Order;
+import com.mealmate.backend.entity.OrderStatus;
+import com.mealmate.backend.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/restaurants")
+public class RestaurantController {
+
+    private OrderRepository orderRepository;
+
+    @PutMapping("/{orderId}/accept")
+    public ResponseEntity<Order> acceptOrder(@PathVariable Long orderId) {
+        Order order = orderRepository.findById(orderId).orElseThrow();
+        order.setStatus(OrderStatus.ACCEPTED);
+        return ResponseEntity.ok(orderRepository.save(order));
+    }
+}
+
