aa3ae0e82df87512e59e660b65b9919790848e71
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-20 12:22:38 +0530

    created markAsDelivered() method in RiderController class

---
+++ src/main/java/com/mealmate/backend/controller/RiderController.java
@@ -0,0 +1,26 @@
+package com.mealmate.backend.controller;
+
+import com.mealmate.backend.entity.Order;
+import com.mealmate.backend.entity.OrderStatus;
+import com.mealmate.backend.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/riders")
+public class RiderController {
+
+    private OrderRepository orderRepository;
+
+    @PutMapping("/{orderId}/deliver")
+    public ResponseEntity<Order> markAsDelivered(@PathVariable Long orderId) {
+        Order order = orderRepository.findById(orderId).orElseThrow();
+        order.setStatus(OrderStatus.DELIVERED);
+        return ResponseEntity.ok(orderRepository.save(order));
+    }
+}
