532592f37b87163848879e242d9bdeacbdd7718d
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-20 12:19:05 +0530

    created placeOrder() method in OrderController class

---
+++ src/main/java/com/mealmate/backend/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.mealmate.backend.controller;
+
+import com.mealmate.backend.entity.Order;
+import com.mealmate.backend.entity.OrderStatus;
+import com.mealmate.backend.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private OrderRepository orderRepository;
+
+    @PostMapping
+    public ResponseEntity<Order> placeOrder(@RequestBody Order order) {
+        order.setStatus(OrderStatus.PENDING);
+        return ResponseEntity.ok(orderRepository.save(order));
+    }
+}
