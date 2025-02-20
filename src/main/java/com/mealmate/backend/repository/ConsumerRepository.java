0c50392876e437713b9f83cf7edd1e9e52fafa60
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-20 20:08:49 +0530

    added two methods findByIdWithAddresses() and findByNameContainingIgnoreCase() in ConsumerRepository interface

--- src/main/java/com/mealmate/backend/repository/ConsumerRepository.java
+++ src/main/java/com/mealmate/backend/repository/ConsumerRepository.java
@@ -1,8 +1,19 @@
 package com.mealmate.backend.repository;
 
 import com.mealmate.backend.entity.Consumer;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource
-public interface ConsumerRepository extends JpaRepository<Consumer, Long> {}
+public interface ConsumerRepository extends BaseRepository<Consumer, Long> {
+
+    @Query("SELECT c FROM Consumer c LEFT JOIN FETCH c.addresses WHERE c.id = :id")
+    Optional<Consumer> findByIdWithAddresses(@Param("id") Long id);
+
+    Page<Consumer> findByNameContainingIgnoreCase(@Param("name") String name, Pageable pageable);
+}
