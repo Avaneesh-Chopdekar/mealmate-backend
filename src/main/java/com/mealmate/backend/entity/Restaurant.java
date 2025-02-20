299b23838690c6f9e974bd982d0f5207f0891804
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-20 19:36:55 +0530

    improved Restaurant entity by added right validations and adding List<MenuItem> menuItems field

--- src/main/java/com/mealmate/backend/entity/Restaurant.java
+++ src/main/java/com/mealmate/backend/entity/Restaurant.java
@@ -1,14 +1,23 @@
 package com.mealmate.backend.entity;
 
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @DiscriminatorValue("RESTAURANT")
 public class Restaurant extends User {
+
+    @NotBlank
+    @Lob
     private String location;
+
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<MenuItem> menuItems = new ArrayList<>();
 }
