f9bc2d52b7dc2677c6bfcfbd4d885c99e111c165
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-20 19:41:40 +0530

    created OrderItem entity class

---
+++ src/main/java/com/mealmate/backend/entity/OrderItem.java
@@ -0,0 +1,31 @@
+package com.mealmate.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "order_items")
+public class OrderItem extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_item_id", nullable = false)
+    private MenuItem menuItem;
+
+    @NotNull
+    @Min(1)
+    private Integer quantity;
+
+    @NotNull
+    @DecimalMin("0.1")
+    private Double price;
+}
