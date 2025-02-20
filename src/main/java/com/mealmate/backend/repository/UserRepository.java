14105c6de76762add00c092215500de444b41f6d
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-20 19:49:45 +0530

    added two methods findByRole() and findByEmailIgnoreCase() on UserRepository interface

--- src/main/java/com/mealmate/backend/repository/UserRepository.java
+++ src/main/java/com/mealmate/backend/repository/UserRepository.java
@@ -1,8 +1,17 @@
 package com.mealmate.backend.repository;
 
+import com.mealmate.backend.entity.Role;
 import com.mealmate.backend.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+import java.util.Optional;
+
 @RepositoryRestResource
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends BaseRepository<User, Long> {
+
+    List<User> findByRole(Role role, Pageable pageable);
+
+    Optional<User> findByEmailIgnoreCase(String email);
+}
