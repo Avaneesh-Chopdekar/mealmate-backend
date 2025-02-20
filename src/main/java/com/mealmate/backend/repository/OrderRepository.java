e2134cdf0e22feae6d90e8f8b324eec9225ba021
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-20 12:13:03 +0530

    created OrderRepository interface

---
+++ src/main/java/com/mealmate/backend/repository/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.mealmate.backend.repository;
+
+import com.mealmate.backend.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface OrderRepository extends JpaRepository<Order, Long> {}
