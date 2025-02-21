6c148783abd9ee194c833250efece72178da5597
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-21 18:52:46 +0530

    created SignupRequest dto class

---
+++ src/main/java/com/mealmate/backend/dto/SignupRequest.java
@@ -0,0 +1,28 @@
+package com.mealmate.backend.dto;
+
+import com.mealmate.backend.entity.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+
+    @NotBlank
+    @Email
+    @Size(max = 100)
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 100)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
