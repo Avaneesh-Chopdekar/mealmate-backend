02ee2b14bd9543489492034faf3c3f299e9bf53d
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-21 16:58:20 +0530

    added filterChain.doFilter(request, response) at the end of doFilterChain() method in JwtAuthenticationFilter class

--- src/main/java/com/mealmate/backend/config/JwtAuthenticationFilter.java
+++ src/main/java/com/mealmate/backend/config/JwtAuthenticationFilter.java
@@ -40,5 +40,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
+
+        filterChain.doFilter(request, response);
     }
 }
