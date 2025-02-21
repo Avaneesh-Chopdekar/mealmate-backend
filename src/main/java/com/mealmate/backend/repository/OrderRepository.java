27e46eb44a1adf70eca2ce0747bffa85d01d4670
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-21 18:54:10 +0530

    fixed JPQL query for findByConsumerIdWithItems() method and changed return type to Page<> from List<>

--- src/main/java/com/mealmate/backend/repository/OrderRepository.java
+++ src/main/java/com/mealmate/backend/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.mealmate.backend.repository;
 
 import com.mealmate.backend.entity.Order;
 import com.mealmate.backend.entity.OrderStatus;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,9 +15,9 @@ public interface OrderRepository extends BaseRepository<Order, Long> {
 
     List<Order> findByStatus(@Param("status") OrderStatus status, Pageable pageable);
 
-    @Query("SELECT o FROM Order o JOIN FETCH o.orderItems WHERE o.consumer.id = :consumerId")
-    List<Order> findByConsumerIdWithItems(@Param("consumerId") Long consumerId, Pageable pageable);
+    @Query("SELECT o FROM Order o JOIN o.orderItems WHERE o.consumer.id = :consumerId")
+    Page<Order> findByConsumerIdWithItems(@Param("consumerId") Long consumerId, Pageable pageable);
 
     @Query("SELECT o FROM Order o WHERE o.rider.id = :riderId")
-    List<Order> findByRiderId(@Param("riderId") Long riderId, Pageable pageable);
+    Page<Order> findByRiderId(@Param("riderId") Long riderId, Pageable pageable);
 }
