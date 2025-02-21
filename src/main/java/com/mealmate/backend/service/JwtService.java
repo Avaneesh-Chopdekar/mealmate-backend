58b6802eda27268de8fa895eaf773e0aabf86237
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-21 11:54:36 +0530

    created JwtService for creating and extracting jwt token

---
+++ src/main/java/com/mealmate/backend/service/JwtService.java
@@ -0,0 +1,52 @@
+package com.mealmate.backend.service;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${app.jwt.secret}")
+    public static String SECRET_KEY;
+    @Value("${app.jwt.access-token-expiration}")
+    public static long ACCESS_TOKEN_EXPIRATION;
+    @Value("${app.jwt.refresh-token-expiration}")
+    public static long REFRESH_TOKEN_EXPIRATION;
+
+    public String generateToken(String username, boolean isAccessToken) {
+        long expiration = isAccessToken ? ACCESS_TOKEN_EXPIRATION : REFRESH_TOKEN_EXPIRATION;
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(SECRET_KEY.getBytes())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
