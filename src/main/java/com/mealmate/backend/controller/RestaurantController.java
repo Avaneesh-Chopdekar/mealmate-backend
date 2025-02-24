0a62779f1285c537f1e8301c3767e2e805007ba7
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-24 21:43:31 +0530

    added @RepositoryRestController and @Tag with meaningful name to RestaurantController class

--- src/main/java/com/mealmate/backend/controller/RestaurantController.java
+++ src/main/java/com/mealmate/backend/controller/RestaurantController.java
@@ -3,23 +3,25 @@ package com.mealmate.backend.controller;
 import com.mealmate.backend.entity.Order;
 import com.mealmate.backend.entity.OrderStatus;
 import com.mealmate.backend.repository.OrderRepository;
+import com.mealmate.backend.repository.RestaurantRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+@Tag(name = "Restaurant API")
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/restaurants")
+@RepositoryRestController
 public class RestaurantController {
 
-    private OrderRepository orderRepository;
+    private final OrderRepository orderRepository;
 
-    @PutMapping("/{orderId}/accept")
+    @PutMapping("/restaurants/{orderId}/accept")
     public ResponseEntity<Order> acceptOrder(@PathVariable UUID orderId) {
         Order order = orderRepository.findById(orderId).orElseThrow();
         order.setStatus(OrderStatus.ACCEPTED);
