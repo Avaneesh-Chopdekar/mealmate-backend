b32c33537091567117c0f2a4e2ede4739834c92a
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-24 21:36:29 +0530

    linked OrderController class with OrderRepository methods using @RepositoryRestController annotation

--- src/main/java/com/mealmate/backend/controller/OrderController.java
+++ src/main/java/com/mealmate/backend/controller/OrderController.java
@@ -3,23 +3,35 @@ package com.mealmate.backend.controller;
 import com.mealmate.backend.entity.Order;
 import com.mealmate.backend.entity.OrderStatus;
 import com.mealmate.backend.repository.OrderRepository;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
+@Tag(name = "Order API")
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/orders")
+@RepositoryRestController
 public class OrderController {
 
-    private OrderRepository orderRepository;
+    private final OrderRepository orderRepository;
 
-    @PostMapping("/place")
+    @PostMapping("/orders")
     public ResponseEntity<Order> placeOrder(@RequestBody Order order) {
         order.setStatus(OrderStatus.PENDING);
         return ResponseEntity.ok(orderRepository.save(order));
+    }
+
+    @PatchMapping("/orders/{id}/cancel")
+    public ResponseEntity<?> cancelOrder(@PathVariable UUID id) {
+        return orderRepository.findById(id)
+                .map(order -> {
+                    order.setStatus(OrderStatus.CANCELLED);
+                    orderRepository.save(order);
+                    return ResponseEntity.ok().build();
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
