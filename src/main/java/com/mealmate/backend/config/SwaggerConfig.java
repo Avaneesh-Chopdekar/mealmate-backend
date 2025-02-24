def922c612a59a20a8e5b1ec08b69b69c860b2c0
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-24 19:46:30 +0530

    added server urls for local developement and production server in SwaggerConfig class

--- src/main/java/com/mealmate/backend/config/SwaggerConfig.java
+++ src/main/java/com/mealmate/backend/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.mealmate.backend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -15,6 +18,15 @@ public class SwaggerConfig {
                         .title("MealMate Backend API")
                         .description("A Food Delivery Application by Avaneesh Chopdekar")
                         .version("1.0.0")
-        );
+        )
+                .servers(
+                        List.of(
+                                new Server().url("http://localhost:8080")
+                                        .description("Local Development Server"),
+                                new Server().url("https://mealmate-backend.onrender.com")
+                                        .description("Production Server")
+                        )
+                ); // TODO: Change production server url after deployment
+
     }
 }
