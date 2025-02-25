8fe0ccfc95384ca7f13bbdd7039cd095cbf912ba
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-25 19:56:01 +0530

    added exception handling in signup() method in AuthController class

--- src/main/java/com/mealmate/backend/controller/AuthController.java
+++ src/main/java/com/mealmate/backend/controller/AuthController.java
@@ -24,49 +24,53 @@ import java.util.Optional;
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
-    private final UserDetailsService userDetailsService;
     private final JwtService jwtService;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody SignupRequest request) {
-        Optional<User> existingUser = userRepository.findByEmail(request.getEmail());
-        if (existingUser.isPresent()) {
-            return ResponseEntity.badRequest().body("Email is already in use.");
+        try {
+            Optional<User> existingUser = userRepository.findByEmail(request.getEmail());
+            if (existingUser.isPresent()) {
+                return ResponseEntity.badRequest().body("Email is already in use.");
+            }
+
+            User newUser;
+            switch (request.getRole()) {
+                case CONSUMER:
+                    newUser = new Consumer();
+                    break;
+                case RIDER:
+                    newUser = new Rider();
+                    break;
+                case RESTAURANT:
+                    newUser = new Restaurant();
+                    break;
+                case ADMIN:
+                    newUser = new Admin();
+                    break;
+                default:
+                    return ResponseEntity.badRequest().body("Invalid role provided.");
+            }
+
+            String accessToken = jwtService.generateToken(request.getEmail(), true);
+            String refreshToken = jwtService.generateToken(request.getEmail(), false);
+
+            newUser.setName(request.getName());
+            newUser.setEmail(request.getEmail());
+            newUser.setPassword(passwordEncoder.encode(request.getPassword()));
+            newUser.setRole(request.getRole());
+            newUser.setRefreshToken(refreshToken);
+
+            userRepository.save(newUser);
+
+            JwtResponse response = new JwtResponse(accessToken, refreshToken, UserDto.fromEntity(newUser));
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            // e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
         }
-
-        User newUser;
-        switch (request.getRole()) {
-            case CONSUMER:
-                newUser = new Consumer();
-                break;
-            case RIDER:
-                newUser = new Rider();
-                break;
-            case RESTAURANT:
-                newUser = new Restaurant();
-                break;
-            case ADMIN:
-                newUser = new Admin();
-                break;
-            default:
-                return ResponseEntity.badRequest().body("Invalid role provided.");
-        }
-
-        String accessToken = jwtService.generateToken(request.getEmail(), true);
-        String refreshToken = jwtService.generateToken(request.getEmail(), false);
-
-        newUser.setName(request.getName());
-        newUser.setEmail(request.getEmail());
-        newUser.setPassword(passwordEncoder.encode(request.getPassword()));
-        newUser.setRole(request.getRole());
-        newUser.setRefreshToken(refreshToken);
-
-        userRepository.save(newUser);
-
-        JwtResponse response = new JwtResponse(accessToken, refreshToken, UserDto.fromEntity(newUser));
-        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/login")
@@ -84,22 +88,22 @@ public class AuthController {
 
     @PostMapping("/refresh-token")
     public ResponseEntity<?> refreshToken(@RequestBody RefreshTokenRequest request) {
-        if (jwtService.validateToken(request.refreshToken())) {
-            String username = jwtService.extractUsername(request.refreshToken());
-            User user = userRepository.findByEmail(username).get();
-
-            if (user.getRefreshToken() == null || !user.getRefreshToken().equals(request.refreshToken())) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token");
-            }
-
-            String accessToken = jwtService.generateToken(username, true);
-            String refreshToken = jwtService.generateToken(username, false);
-            user.setRefreshToken(refreshToken);
-            JwtResponse response = new JwtResponse(accessToken, refreshToken, UserDto.fromEntity(user));
-            return ResponseEntity.ok(response);
-        } else {
+        if (!jwtService.validateToken(request.refreshToken())) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token");
         }
+
+        String username = jwtService.extractUsername(request.refreshToken());
+        User user = userRepository.findByEmail(username).get();
+
+        if (user.getRefreshToken() == null || !user.getRefreshToken().equals(request.refreshToken())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid refresh token");
+        }
+
+        String accessToken = jwtService.generateToken(username, true);
+        String refreshToken = jwtService.generateToken(username, false);
+        user.setRefreshToken(refreshToken);
+        JwtResponse response = new JwtResponse(accessToken, refreshToken, UserDto.fromEntity(user));
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/logout")
