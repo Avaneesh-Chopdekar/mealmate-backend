df9daa5997a3a06fa02d974ee514efda214f0b82
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-02-28 23:04:57 +0530

    added findByUserId() method in FavoriteRepository interface

--- src/main/java/com/mealmate/backend/repository/FavoriteRepository.java
+++ src/main/java/com/mealmate/backend/repository/FavoriteRepository.java
@@ -1,4 +1,14 @@
 package com.mealmate.backend.repository;
 
-public class FavoriteRepository {
+import com.mealmate.backend.entity.Favorite;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+@RepositoryRestResource(collectionResourceRel = "favorites", path = "favorites")
+public interface FavoriteRepository extends BaseRepository<Favorite, UUID> {
+    List<Favorite> findByUserId(UUID userId);
 }
